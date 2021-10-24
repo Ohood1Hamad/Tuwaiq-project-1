5b51daa875084f2a88eb4ac6a91b309636d06b3a
[Ohood1Hamad] <[Ohood.Hamad.eam.js@tuwaiq.edu.sa]>
2021-10-25 02:39:47 +0300

    2nd UpDate of the 1project

--- project1/JS/flowerall.js
+++ project1/JS/flowerall.js
@@ -1,6 +1,24 @@
-$(".munu-toggle-btn").click(function (e) {
-    $("this").toggleclass("fa-time");
-    const newLocal = "active";
-    $(".navigation-menu").toggleclass(newLocal);
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if($(this).scrollTop()> 40 ){
+            $('#topBtn').fadeiIn();
+        } else{
+            $('#topBtn').fadeout();
+        }
+
+        
+    })
+    $("#topBtn").click(function () {
+        
+        $('html ,body').animate({scrollTop :0},800);
+
+    })
     
-});
+})
+
+// $(".munu-toggle-btn").click(function (e) {
+//     $("this").toggleclass("fa-time");
+//     const newLocal = "active";
+//     $(".navigation-menu").toggleclass(newLocal);
+    
+// });
